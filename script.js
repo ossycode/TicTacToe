14df72c507a166db5f579552b96ebee8b37e6f74
Ossy Lab <ossyisibor@gmail.com>
2022-10-11 15:03:41 +0100

    UI Setup

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+const titleScreen = (() => {
+
+    // Saving HTML DOM elements to be used 
+    const startbutton = document.querySelector('.startGame');
+    const cardplayer = document.querySelector('.player');
+    const cardbot = document.querySelector('.bot');
+    const vs = document.querySelector('.vs');
+    const titleContainer = document.querySelector('.main');
+    const header = document.querySelector('header');
+
+    //Button animatios for looding screen
+    
+})
